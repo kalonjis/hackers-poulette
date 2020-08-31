8dde0d12178890e4d36b06134986dd35ec0c3364
Stephane <kalonjistephane@gmail.com>
2020-08-31 12:21:57 +0200

    hackerspoulsteph

--- mail.php
+++ mail.php
@@ -17,8 +17,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'kalonjistephane@gmail.com';                     // SMTP username
-    $mail->Password   = 'Bulabs1981';                               // SMTP password
+    $mail->Username   = 'hackerpoulsteph@gmail.com';                     // SMTP username
+    $mail->Password   = 'Becode2020';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
